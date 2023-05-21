31c0e27d5a3a449fbfe4380d826ceb7e23169964
t <t@mail.com>
2023-05-21 13:16:37 +0900

    done

--- templates/single/dev/App.ts
+++ templates/single/dev/App.ts
@@ -13,11 +13,11 @@ export default <% if (version === 3) {%>defineComponent<% } else { %>Vue.extend<
 
   metaInfo: {
     link: [
-      { href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css', rel: 'stylesheet' },
-      { href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css', rel: 'stylesheet' },
+      { href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css', rel: 'stylesheet' },
+      { href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css', rel: 'stylesheet' },
     ],
     script: [
-      { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js' },
+      { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js' },
     ]
   },
 });
