849fb52814446bb46ab3a248cfcc20b20cd4becc
t <t@mail.com>
2023-08-11 13:39:20 +0900

    done

--- templates/store.module/actions.js
+++ templates/store.module/actions.js
@@ -1,2 +1,67 @@
 export default {
+  list: firestoreAction(function (ctx) {
+    const list_ref = ctx.getters['LIST_REF']
+
+    try {
+      return ctx.bindFirestoreRef('list', list_ref)
+    } catch (e) {
+      return this.add_error(e)
+    }
+  }),
+  
+  get: firestoreAction(function (ctx, id) {
+    const item_ref = ctx.getters['LIST_REF'].doc(id)
+  
+    try {
+      return ctx.bindFirestoreRef('item', item_ref)
+    } catch (e) {
+      return this.add_error(e)
+    }
+  }),
+  
+  add (ctx, item) {
+    const list_ref = ctx.getters['LIST_REF']
+    item.created_at = ctx.rootGetters['firebase/FNS'].Timestamp.server()
+  
+    try {
+      return list_ref.add(item)
+    } catch (e) {
+      return this.add_error(e)
+    }
+  },
+  
+  remove (ctx, id) {
+    const item_ref = ctx.getters['LIST_REF'].doc(id)
+  
+    try {
+      return item_ref.delete()
+    } catch (e) {
+      return this.add_error(e)
+    }
+  },
+  
+  update (ctx, { id, ...rest }) {
+    id = id || ctx.state.item.id
+    const list_ref = ctx.getters['LIST_REF']
+
+    let err
+    if (! id)
+      err = 'No id to update'
+    else if (! list_ref)   
+      err = 'No reference to update data'
+    else if (! rest)  
+      err = 'No data to update'
+    else if (typeof rest != 'object') 
+      err = 'Invalid data type'
+  
+    if (err) {
+      return Promise.reject(err)
+    } else {
+      try {
+        return list_ref.doc(id).update(rest)
+      } catch (e) {
+        return this.add_error(e)
+      }
+    }
+  }
 }
--- templates/store.module/getters.js
+++ templates/store.module/getters.js
@@ -1,2 +1,22 @@
+const TARGET = 'bs';
+
 export default {
+  LIST_REF (state, getters, rootState, rootGetters) {
+    if (_.has(rootGetters, 'firebase/ROOT_REF')) {
+      return rootGetters['firebase/ROOT_REF'].collection(TARGET)
+    }
+  },
+  
+  ITEM_REF (state, getters) {
+    const list_ref = getters['LIST_REF']
+    const id = state.item.id
+  
+    if (list_ref && id) {
+      return list_ref.doc(id)
+    }
+  },
+  
+  ORDERED_LIST (state) {
+    return _.orderBy(state.list, [ 'created_at.seconds' ], [ 'asc' ])
+  }
 }
